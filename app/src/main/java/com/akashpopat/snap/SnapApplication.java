ad039440424ded82b99997dbcd9c3ffe4445d495
Akash Popat <akash.popat@gmail.com>
2015-10-09 06:54:15 +0530

    before parse

---
+++ app/src/main/java/com/akashpopat/snap/SnapApplication.java
@@ -0,0 +1,15 @@
+package com.akashpopat.snap;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+/**
+ * Created by akash on 10/9/2015.
+ */
+public class SnapApplication extends Application {
+
+    // Enable Local Datastore.
+
+
+}
