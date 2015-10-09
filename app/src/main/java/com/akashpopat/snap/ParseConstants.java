1b1d64b03d9f23be403bb1cf2fc5bdcfe888654b
Akash Popat <akash.popat@gmail.com>
2015-10-09 22:48:35 +0530

    Done with users

---
+++ app/src/main/java/com/akashpopat/snap/ParseConstants.java
@@ -0,0 +1,11 @@
+package com.akashpopat.snap;
+
+/**
+ * Created by akash on 10/9/2015.
+ */
+public final class ParseConstants {
+
+    // Field names
+    public static final String KEY_USERNAME = "username";
+    public static final String KEY_FRIENDS_RELATION = "friendsRelation";
+}
