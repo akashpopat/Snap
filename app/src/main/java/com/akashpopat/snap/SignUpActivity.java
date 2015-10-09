92aea5bc4aefc3870e92dbbf83caa6e70a0fdad1
Akash Popat <akash.popat@gmail.com>
2015-10-09 08:10:42 +0530

    Basic user creation from sign up

--- app/src/main/java/com/akashpopat/snap/SignUpActivity.java
+++ app/src/main/java/com/akashpopat/snap/SignUpActivity.java
@@ -1,13 +1,82 @@
 package com.akashpopat.snap;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    protected EditText mUsernmae;
+    protected EditText mPassword;
+    protected EditText mEmail;
+    protected Button mSignUpBotton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mUsernmae = (EditText) findViewById(R.id.usernameField);
+        mPassword = (EditText) findViewById(R.id.passwordField);
+        mEmail = (EditText) findViewById(R.id.emailField);
+        mSignUpBotton = (Button) findViewById(R.id.signupButton);
+
+        mSignUpBotton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = mUsernmae.getText().toString().trim();
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                if(username.isEmpty() || email.isEmpty() || password.isEmpty())
+                {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                    builder.setMessage(R.string.sign_up_error_message)
+                            .setTitle(R.string.sign_up_error_title)
+                            .setPositiveButton(android.R.string.ok,null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+                else
+                {
+                    ParseUser newUser = new ParseUser();
+                    newUser.setUsername(username);
+                    newUser.setPassword(password);
+                    newUser.setEmail(email);
+                    newUser.signUpInBackground(new SignUpCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if(e == null){
+                                // Success!
+                                Intent intent = new Intent(SignUpActivity.this,MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(intent);
+                            }
+                            else {
+                                // Didnt work
+                                AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                                builder.setMessage(e.getMessage())
+                                        .setTitle(R.string.sign_up_error_title)
+                                        .setPositiveButton(android.R.string.ok,null);
+                                AlertDialog dialog = builder.create();
+                                dialog.show();
+                            }
+                        }
+                    });
+
+                }
+
+            }
+        });
+
     }
 }
