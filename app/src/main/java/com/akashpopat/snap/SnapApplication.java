391ea16e492fde4ef9c173badfebe667178f6d9d
Akash  Popat <akashpopat@gmail.com>
2015-10-30 01:13:05 +0530

    Update SnapApplication.java

--- app/src/main/java/com/akashpopat/snap/SnapApplication.java
+++ app/src/main/java/com/akashpopat/snap/SnapApplication.java
@@ -15,7 +15,7 @@ public class SnapApplication extends Application {
 
         // Enable Local Datastore.
         Parse.enableLocalDatastore(this);
-        Parse.initialize(this, "Vf13WP5vyhoDkkASeXUDzvZXerVTB4iT750RE1bP", "28rMOc8SqBuyUSprME8WeOO0lMQnFl6Lxhvzl5DO");
+        Parse.initialize(this, // YOUR KEY );
 
     }
 }
